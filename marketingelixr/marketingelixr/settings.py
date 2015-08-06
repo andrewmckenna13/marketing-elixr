3884332a35394c4db20086be7227fdb50b594bfa
Ubuntu <ubuntu@ip-172-31-28-14.us-west-2.compute.internal>
2015-08-06 18:45:50 +0000

    andrew test 2

--- marketingelixr/marketingelixr/settings.py
+++ marketingelixr/marketingelixr/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'site',
+    'marketingelixr2',
 )
 
 MIDDLEWARE_CLASSES = (
